e5edcf4bde54c05f6e0c8791dce81a41986363f8
i.yusubov555@gmail.com <i.yusubov555@gmail.com>
2019-06-19 03:41:31 +0400

    removed some files

---
+++ java/main/java/com/test/pm/users/dto/UserDto.java
@@ -0,0 +1,17 @@
+package az.inngress.pm.users.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDto {
+
+    private String firstName;
+    private String lastName;
+    private String email;
+}
