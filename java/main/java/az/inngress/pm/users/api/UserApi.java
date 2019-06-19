21ed74ef773ddf8dc2bfb7214d90ec7de57048b1
i.yusubov555@gmail.com <i.yusubov555@gmail.com>
2019-06-19 12:21:34 +0400

    Some test

--- java/main/java/az/inngress/pm/users/api/UserApi.java
+++
@@ -1,8 +0,0 @@
-package az.inngress.pm.users.api;
-
-import az.inngress.pm.users.dto.UserDto;
-
-public interface UserApi {
-
-    Long createUser(UserDto userDto);
-}
--- java/main/java/az/inngress/pm/users/services/UserService.java
+++
@@ -1,8 +0,0 @@
-package az.inngress.pm.users.services;
-
-import az.inngress.pm.users.dto.UserDto;
-
-public interface UserService {
-
-    Long createUser(UserDto userDto);
-}
